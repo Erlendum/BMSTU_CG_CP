1bb7f7231a07bbd25122397efc9cc420dcaf6206
Erlendum <erlendum.ds@gmail.com>
2022-12-04 18:11:36 +0300

    scripts were updated.

--- scripts/creating_images_03.sh
+++ scripts/creating_images_03.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-r_values="568 576 584 592 600 608 616 624 632 640 648 656 664 672 680 688 696 704 712 720"
+r_values="526 534 552 560 568 576 584 592 600 608 616 624 632 640 648 656 664 672 680 688 696 704 712"
 
 if [ ! -z $1 ]; then
 	r_values=$1
@@ -13,7 +13,7 @@ echo "Creating images..."
 for r in $r_values; do
 	echo -n -e "r=$r\r"
 	r_with_zeros=$(printf "%03d" $r)
-	./project1 -s scenes/scene_10 -ry "$r" -o ../../data/"$r_with_zeros".png
+	./project1 -s scenes/scene_16 -ry "$r" -o ../../data/"$r_with_zeros".png
 done
 
 
